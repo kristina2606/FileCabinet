b03d9124d6e6ca2c776850c69949efbb137ac895
Kristina Loboda <kris26tina06@gmail.com>
2023-02-26 14:44:25 +0100

    Fix unnecessary zero-length array allocation in FileCabinetService.cs.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,13 @@
+namespace FileCabinetApp
+{
+    public class FileCabinetRecord
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
